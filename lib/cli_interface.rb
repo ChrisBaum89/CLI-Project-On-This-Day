ae8088c801052beb5f3b6f89a96fd3c855778267
Chris Baum <Christopher.Baum@valpo.edu>
2021-02-28 23:00:21 -0500

    cleaned up syntax errors

--- lib/cli_interface.rb
+++ lib/cli_interface.rb
@@ -1,6 +1,8 @@
 require_relative "../lib/scraper.rb"
+require_relative "../lib/month.rb"
 require_relative "../lib/event.rb"
-require 'nokogiri'
+#require 'nokogiri'
+require 'pry'
 
 #class will allow a user to select a month and day
 #class will allow a user to select today
@@ -10,28 +12,21 @@ class CommandLineInterface
     puts "By inputing a month and a day, we can learn about notable events that happened on that day!"
     puts "Please enter 1 if you would like to learn about an event in history today."
     puts "Please enter 2 if you would like to select a month and day."
+    puts "----------------------------------------------------------"
     input = gets.strip
-    user selection
+    user_selection(input)
   end
 
-  def user_selection
-    if input == 1
+  def user_selection(input)
+    if input == "1"
       @month = time.month
       @day = time.day
-    elsif input == 2
-      puts "Select a month by entering the number that coresponds with the month:"
-      puts "1. January"
-      puts "2. February"
-      puts "3. March"
-      puts "4. April"
-      puts "5. May"
-      puts "6. June"
-      puts "7. July"
-      puts "8. August"
-      puts "9. September"
-      puts "10. October"
-      puts "11. November"
-      puts "12. December"
+    elsif input == "2"
+      puts "Select a month by entering the number that corresponds with the month:"
+      Month.all.each do |x|
+        puts "#{x.number}. #{x.name}"
+      end
+      puts "----------------------------------------------------------"
       @month = gets.strip
       puts "You selected #{month.capitalize}. Please select a day."
       @day = gets.strip
@@ -48,18 +43,18 @@ class CommandLineInterface
 
   #sets the months
   def months
-    Month("January", 1, 31)
-    Month("February", 2, 29)
-    Month("March", 3, 31)
-    Month("April", 4, 30)
-    Month("May", 5, 31)
-    Month("June", 6, 30)
-    Month("July", 7, 31)
-    Month("August", 8, 31)
-    Month("September", 9, 30)
-    Month("October", 10, 31)
-    Month("November", 11, 30)
-    Month("December", 12, 31)
+    Month.new("January", 1, 31)
+    Month.new("February", 2, 29)
+    Month.new("March", 3, 31)
+    Month.new("April", 4, 30)
+    Month.new("May", 5, 31)
+    Month.new("June", 6, 30)
+    Month.new("July", 7, 31)
+    Month.new("August", 8, 31)
+    Month.new("September", 9, 30)
+    Month.new("October", 10, 31)
+    Month.new("November", 11, 30)
+    Month.new("December", 12, 31)
   end
 
 end
