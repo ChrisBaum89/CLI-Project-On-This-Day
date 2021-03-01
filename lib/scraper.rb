95b82e599c9c70eca773396b19088fdd0c2dc387
Chris Baum <Christopher.Baum@valpo.edu>
2021-02-28 23:18:34 -0500

    added require 'open-uri'.  Wasn't obtaining the html from the website and this was due to the 'open-uri' missing

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,5 @@
 require 'pry'
+require 'open-uri'
 require 'nokogiri'
 
 class Scraper
@@ -7,13 +8,15 @@ class Scraper
   def initialize(month, day)
     @month = month
     @day = day
-    hash = {}
-
+    @hash = {}
+    binding.pry
+    obtain_html
   end
 
   def obtain_html
     index_url = "https://en.wikipedia.org/wiki/Wikipedia:Selected_anniversaries/#{@month}"
     @doc = Nokogiri::HTML(open(index_url))
+    binding.pry
   end
 
   def create_events
