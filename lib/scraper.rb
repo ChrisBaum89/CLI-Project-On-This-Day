7055ebc4cfcb8419d10c15a24a5d1901a56ba338
Chris Baum <Christopher.Baum@valpo.edu>
2021-02-28 22:10:07 -0500

    scraper.rb - created initialize with needed tags, created obtain_html

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,5 +2,21 @@ require 'pry'
 require 'nokigir'
 
 class Scraper
+  attr_reader :month, :day
 
+  def initialize(month, day)
+    @month = month
+    @day = day
+    hash = {}
+
+  end
+
+  def obtain_html
+    index_url = "https://en.wikipedia.org/wiki/Wikipedia:Selected_anniversaries/#{@month}"
+    @doc = Nokogiri::HTML(open(index_url))
+  end
+
+  def create_events
+
+  end
 end
