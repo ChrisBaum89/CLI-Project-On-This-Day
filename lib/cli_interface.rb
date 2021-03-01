ba3a156ed1f275a85b0c17bbe146940dc4f420fc
Chris Baum <Christopher.Baum@valpo.edu>
2021-02-28 22:37:31 -0500

    Added method Months that creates the month objects

--- lib/cli_interface.rb
+++ lib/cli_interface.rb
@@ -6,6 +6,7 @@ require 'nokogiri'
 #class will allow a user to select today
 class CommandLineInterface
   def run
+    months
     puts "By inputing a month and a day, we can learn about notable events that happened on that day!"
     puts "Please enter 1 if you would like to learn about an event in history today."
     puts "Please enter 2 if you would like to select a month and day."
@@ -18,7 +19,19 @@ class CommandLineInterface
       @month = time.month
       @day = time.day
     elsif input == 2
-      puts "Select a month:"
+      puts "Select a month by entering the number that coresponds with the month:"
+      puts "1. January"
+      puts "2. February"
+      puts "3. March"
+      puts "4. April"
+      puts "5. May"
+      puts "6. June"
+      puts "7. July"
+      puts "8. August"
+      puts "9. September"
+      puts "10. October"
+      puts "11. November"
+      puts "12. December"
       @month = gets.strip
       puts "You selected #{month.capitalize}. Please select a day."
       @day = gets.strip
@@ -31,6 +44,22 @@ class CommandLineInterface
   def incorrect_selection
     puts "Selection not available, please try again."
     run
+  end
+
+  #sets the months
+  def months
+    Month("January", 1, 31)
+    Month("February", 2, 29)
+    Month("March", 3, 31)
+    Month("April", 4, 30)
+    Month("May", 5, 31)
+    Month("June", 6, 30)
+    Month("July", 7, 31)
+    Month("August", 8, 31)
+    Month("September", 9, 30)
+    Month("October", 10, 31)
+    Month("November", 11, 30)
+    Month("December", 12, 31)
   end
 
 end
