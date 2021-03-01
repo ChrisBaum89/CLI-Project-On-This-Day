ba2340d7034a3cfea8c559231f912e08d7deb194
Chris Baum <Christopher.Baum@valpo.edu>
2021-02-28 22:25:17 -0500

    Developing messaging and starting to handle incorrect selections

--- lib/cli_interface.rb
+++ lib/cli_interface.rb
@@ -8,12 +8,29 @@ class CommandLineInterface
   def run
     puts "By inputing a month and a day, we can learn about notable events that happened on that day!"
     puts "Please enter 1 if you would like to learn about an event in history today."
-    puts "Please enter 2 if you would like to select a montha and day."
-
-    puts "Select a month"
-    month = gets.strip
-    puts "You selected #{month.capitalize}". Please select a day."
-
-    Scraper.new(input)
+    puts "Please enter 2 if you would like to select a month and day."
+    input = gets.strip
+    user selection
   end
+
+  def user_selection
+    if input == 1
+      @month = time.month
+      @day = time.day
+    elsif input == 2
+      puts "Select a month:"
+      @month = gets.strip
+      puts "You selected #{month.capitalize}. Please select a day."
+      @day = gets.strip
+    else
+      incorrect_selection
+    end
+    Scraper.new(@month, @day)
+  end
+
+  def incorrect_selection
+    puts "Selection not available, please try again."
+    run
+  end
+
 end
