fff3d4d41e5049b7867cc9c20256947566cc9a13
Chris Baum <Christopher.Baum@valpo.edu>
2021-02-28 22:55:25 -0500

    ensured arguments/attributes are accesssible. fixed error with all method (should be self.all, should just be returning @@all) loading of object into class variable @@all changed to occur in intialize.

--- lib/month.rb
+++ lib/month.rb
@@ -1,13 +1,16 @@
 class Month
-  attr_reader :name, :number
+  attr_reader :name, :number, :number_of_days
 
   @@all = []
   def initialize(name, number, number_of_days)
+    @name = name
     @number = number
+    @number_of_days = number_of_days
+    @@all << self
   end
 
-  def all
-    @all << self
+  def self.all
+    @@all
   end
 
 end
