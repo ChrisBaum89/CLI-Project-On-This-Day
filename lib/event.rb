56cd758e06ab6f1ded2eb4ea717119cfe6685368
Chris Baum <Christopher.Baum@valpo.edu>
2021-03-01 19:20:43 -0500

    initial coding of the Event class.  Inlcuded attr_reader, intialize method and .all method

--- lib/event.rb
+++ lib/event.rb
@@ -1,5 +1,20 @@
 require 'pry'
 
 class Event
+  attr_reader :month, :day, :year, :description
+
+  @@all = []
+
+  def initialize(hash)
+    @month = hash(:month)
+    @day = hash(:day)
+    @year = hash(:year)
+    @description = hash(:description)
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 
 end
